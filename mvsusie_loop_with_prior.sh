40d0259d1e487d54abf3bca761430aed3a575bbb
Alfonso Martone <alfonso.martone96@gmail.com>
2025-12-30 16:37:31 +0100

    Modify output paths and email in mvsusie_loop_with_prior.sh
    Updated file paths and email address in SLURM script.

--- mvsusie_loop_with_prior.sh
+++ mvsusie_loop_with_prior.sh
@@ -4,14 +4,14 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --time=01:00:00
 #SBATCH --job-name=mvsusie_loop_with_prior
-#SBATCH --output=/home/amartone/colocal/mvsusie_loop_with_prior.%A.out 
-#SBATCH --error=/home/amartone/colocal/mvsusie_loop_with_prior.%A.err 
+#SBATCH --output=/path/to/mvsusie/results/mvsusie_loop_with_prior.%A.out 
+#SBATCH --error=/path/to/mvsusie/results/mvsusie_loop_with_prior.%A.err 
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=alfonso.martone@radboudumc.nl
+#SBATCH --mail-user=XXX
 
-lava_results_df="/home/amartone/colocal/all_pheno_pairs.all_loci.results.bivar.lava.fdr_sign.tsv"
+lava_results_df="/path/to/all_pheno_pairs.all_loci.results.bivar.lava.fdr_sign.tsv" 
 zscores="/path/to/zscore/matrix/zscores_matrix.tsv"
-ldsc_output="/home/amartone/genomicsem/genomicsem_cog_IR_all/LDSCoutput_all_pheno_rev_ISI.RData"
+ldsc_output="/path/to/GenomicSEM/LDSCoutput.RData"
 snp_info="/path/to/ref_genotype/g1000_eur_maf001.bim"
 prior_zscores="path/to/zscore/matrix/prior_matrix_zscores.rds"
 path/to/zscore/matrix/zscores_matrix.tsv
